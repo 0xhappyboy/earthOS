66f670e98c6303f333e918669bf8ba0696a4e19b
happyboy <superhappyboy1995@gmail.com>
2026-06-07 07:25:02 +0800

    update : Update the compiled files.

--- core/vite.config.js
+++ core/vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   plugins: [
-    react(),
     dts({
       rollupTypes: true,
       copyDtsFiles: false,
@@ -13,12 +11,9 @@ export default defineConfig({
   ],
   build: {
     lib: {
-      entry: 'src/index.tsx',
+      entry: 'src/index.ts',
       formats: ['es', 'cjs'],
       fileName: (format) => format === 'es' ? 'index.mjs' : 'index.js'
     },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'lightweight-charts']
-    }
   }
 })
